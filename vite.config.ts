77075b4ed2d2a242fc1a036471a4cbcb9eef2a82
Princely2001 <157102478+Princely2001@users.noreply.github.com>
2025-06-08 05:06:35 +0530

    Update vite.config.ts
    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 
+
+
+
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',  // Accept connections from any IP (needed for cloud/EC2)
-    port: 3000,        // Use a standard port (like 3000) instead of just 3
+  server:{
+
+    host:true,
+    port:80,
   },
 })
